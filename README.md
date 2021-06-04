aac5ed2143039d63db0d708b594f55900b400c04
Md. Manjurul Hussain <mmhs013@gmail.com>
2021-06-04 23:09:37 +0600

    update project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MannKendall
 
 [![Build Status](https://travis-ci.com/mmhs013/MannKendall.jl.svg?branch=master)](https://travis-ci.com/mmhs013/MannKendall.jl)
+
+`MannKendall.jl` is a pure julia package for Mann-Kendall trend test. This package is now under development. It will cover all function of [pyMannKendall](https://github.com/mmhs013/pyMannKendall) very soon.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using MannKendall
 using Test
-
+# NoTrendData = ones(360)*rand(Int, 1)
+TrendData = 1:100;
 @testset "MannKendall.jl" begin
     # Write your tests here.
+    TrendRes = mk_original_test(TrendData);
+
+    @test TrendRes.trend == "increasing";
+    @test TrendRes.h == true;
+    @test TrendRes.p == 0.0;
+    @test TrendRes.z == 14.73869998208331;
+    @test TrendRes.τ == 1.0;
+    @test TrendRes.s == 4950;
+    @test TrendRes.var_s ==112750.0;
+
 end
